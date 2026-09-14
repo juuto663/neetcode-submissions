ddc2369e702f2b6dd85dc1fb3d21b5bbfeef6844
juuto663 <justin.anderson.personal@gmail.com>
2026-09-13 19:53:47 -0500

    Add: validate-parentheses - submission-15

---
+++ Data Structures & Algorithms/validate-parentheses/submission-15.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        bracket_map = {
+            "}" : "{",
+            "]" : "[",
+            ")" : "(",
+        }
+        stack = []
+        for symbol in s:
+            if not stack or symbol not in bracket_map:
+                stack.append(symbol)
+                continue
+            elif stack[-1] == bracket_map.get(symbol, None):
+                stack.pop()
+            else:
+                return False
+        if stack:
+            return False
+        return True
+             
